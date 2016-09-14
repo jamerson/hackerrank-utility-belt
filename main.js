c9d5b45964200386c72cfaeb27d25f5dce7b3b5e
jamerson <jamerson.lima@gmail.com>
2016-09-14 08:54:13 -0300

    more 2-dimensions array functions

--- main.js
+++ main.js
@@ -19,8 +19,65 @@ function countAndLines(input, callback) {
     }
 }
 
-function puts(value) {
-    console.log(value)
+function countAndMatrix(input, callback) {
+    var lines = splitLines(input)
+    var array_size = parseInt(lines[0])
+    var result = new Array(array_size)
+    for(var i = 0; i < array_size; i++) {
+        result[i] = new Array(array_size)
+        for(var j = 0; j < array_size; j++) {
+            result[i][j] = callback(lines[i+1].charAt(j))
+        }
+    }
+    return result
+}
+
+function traverseMatrix(matrix, callback) {
+    var r = matrix.length
+    for(var i = 0; i < r; i++) {
+        traverseLine(matrix, i,  callback)
+    }
+}
+
+function traverseLine(matrix, line_num, callback) {
+    var c = matrix[line_num].length
+    for(var j = 0; j < c; j++) {
+        callback(matrix[line_num][j], line_num, j, matrix)
+    }
+}
+
+function goToDirection(matrix, x, y, direction, stop_at, callback) {
+    var x_offset = 0
+    var y_offset = 0
+
+    switch(direction) {
+        case 'up':
+            x_offset = -1
+            y_offset = 0
+            break
+        case 'down':
+            x_offset = 1
+            y_offset = 0
+            break
+        case 'left':
+            x_offset = 0
+            y_offset = -1
+            break
+        case 'right':
+        default:
+            x_offset = 0
+            y_offset = 1
+            break
+    }
+
+    var stop_flag = false
+    x += x_offset
+    y += y_offset
+    while(x < matrix.length && x >= 0 && y < matrix.length && y >= 0 && stop_at.indexOf(matrix[x][y]) < 0 && !stop_flag) {
+        stop_flag = callback(matrix[x][y], x, y, matrix)
+        x += x_offset
+        y += y_offset
+    }
 }
 
 function result(value) {
@@ -28,9 +85,20 @@ function result(value) {
 }
 
 function processData(input) {
-    countAndLines(input, (line, index) => {
-        var count = parseInt(line)
-        result(count % 2 === 0?'Yes':'No')
+    var matrix = countAndMatrix(input, (value) => {
+        return value
+    })
+    traverseMatrix(matrix, (item, x, y, matrix) => {
+        if(item === '.') {
+            var r_count = 0
+            goToDirection(matrix, x, y, 'right', ['*'], (item, x, y, matrix) => {
+                var result = item === '*';
+                if(!result)
+                    r_count += 1
+                return result
+            })
+            console.log(r_count)
+        }
     })
 }
 
